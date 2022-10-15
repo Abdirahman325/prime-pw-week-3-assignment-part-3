c39abc52a62506c97d38cc9045643976775a1395
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-10-15 07:51:51 -0500

    resubmission complete

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,14 +5,13 @@ console.log('****** Part Supply *******');
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
     
-    let partsNeeded = [40]
-    console.log('number of parts:', partsNeeded)
-
+    let partsNeeded = 40
+    
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 
-    let supplyChangeArray = ['3','5', '-6', '0', '7', '11']
+    let supplyChangeArray = [3, 5, -6, 0, 7, 11]
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
@@ -29,7 +28,7 @@ console.log('4. Removed item:');
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 
-    let addedParts = supplyChangeArray.push('25');
+    let addedParts = supplyChangeArray.push(25);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -39,18 +38,16 @@ console.log('5. Adding 25 to supplyChanges.');
 console.log('6. Showing supplyChanges...');
 
     for ( let i=0; i<supplyChangeArray.length; i++ ){
-        console.log('for loop', supplyChangeArray [ i ]);
-    }
-        if(supplyChangeArray [ i ] > '0' ){
-            console.log( 'Added x parts.');
+        if(supplyChangeArray [ i ] > 0 ){
+            console.log( 'Added' + supplyChangeArray[i] + 'parts.');
         }
-            else if (supplyChangeArray [ i ] === '0'){
+            else if (supplyChangeArray [ i ] === 0){
                 console.log('No Change.')
             }
-                else if(supplyChangeArray [ i ] < '-1'){
-                    console.log('Removed x parts.')
+                else {
+                    console.log('Removed ' + supplyChangeArray[ i ] + ' parts.');
                 }
-    
+            }
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
